ffb1d2db334f6fd3decd2e5c9fdbdc826b3f5097
Cobblestone535 <phoebe.c.navin@gmail.com>
2024-08-07 20:59:32 -0400

    Changed Values

--- AdvantageScopePractice/src/main/java/frc/robot/subsystems/Drivetrain.java
+++ AdvantageScopePractice/src/main/java/frc/robot/subsystems/Drivetrain.java
@@ -40,14 +40,14 @@ public class Drivetrain extends SubsystemBase {
   public static final double ksVolts = 0.01;
   public static final double kvVoltSecondsPerMeter = .2;
   public static final double kaVoltSecondsSquaredPerMeter = 0.1;
-  public static final double kPDriveVel = 8.5;
+  public static final double kPDriveVel = 2;
 
   public static final double kTrackwidthMeters = 0.016;
   public static final DifferentialDriveKinematics kDriveKinematics =
     new DifferentialDriveKinematics(kTrackwidthMeters);
 
-  public static final double kMaxSpeedMetersPerSecond = .53;
-  public static final double kMaxAccelerationMetersPerSecondSquared = .5;
+  public static final double kMaxSpeedMetersPerSecond = .762;
+  public static final double kMaxAccelerationMetersPerSecondSquared = .127;
 
   public static final double kRamseteB = 2;
   public static final double kRamseteZeta = 0.7;
